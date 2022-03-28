9d106bebce4dd0068e2d68ae31b6a20c66e1be52
Hugo Vila <code.hvila@gmail.com>
2022-03-28 14:12:03 +0200

    create shopping list

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Shopping List</h1>
+      <ul className="list">
+        <li className="item">Milk</li>
+        <li className="item">Eggs</li>
+        <li className="item">Bread</li>
+      </ul>
     </div>
   );
 }
