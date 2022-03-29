53b4239152526ecd475a2856ef9b3796e8646ae4
Hugo Vila <code.hvila@gmail.com>
2022-03-29 18:50:39 +0200

    Display Computed Data Using Recoil Selectors

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
 import "./App.css";
-import { atom, useRecoilState } from "recoil";
+import { atom, selector, useRecoilState, useRecoilValue } from "recoil";
+
+const priceList = {
+  Milk: 0.76,
+  Eggs: 1.25,
+  Bread: 0.45,
+  "Olive Oil Bread": 0.5,
+};
 
 const shoppingList = atom({
   key: "shoppingList",
-  default: ["Milk", "Eggs", "Bread"],
+  default: ["Milk", "Eggs", "Bread", "Olive Oil Bread"],
+});
+
+const amount = selector({
+  key: "amount",
+  get: ({ get }) => {
+    return {
+      totalPrice: get(shoppingList)
+        .map((food) => {
+          return priceList[food] || 0;
+        })
+        .reduce((current, sum) => {
+          return sum + current;
+        }, 0),
+      totalItems: get(shoppingList).length,
+    };
+  },
 });
 
 function App() {
   const [myShoppingList, setMyShoppingList] = useRecoilState(shoppingList);
+  const total = useRecoilValue(amount);
 
   const removeItem = (item) => () => {
     const itemIndex = myShoppingList.findIndex((element) => element === item);
@@ -39,6 +63,8 @@ function App() {
           </li>
         ))}
       </ul>
+      <h3>Total Price: {total.totalPrice}</h3>
+      <h4>Total Items: {total.totalItems}</h4>
     </div>
   );
 }
