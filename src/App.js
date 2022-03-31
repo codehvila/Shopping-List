5261cc86fa925d4a5072395425d876cb845df190
Hugo Vila <code.hvila@gmail.com>
2022-03-31 23:52:29 +0200

    create weather form

--- src/App.js
+++ src/App.js
@@ -1,43 +1,27 @@
 import "./App.css";
-import { atom, selectorFamily, useRecoilValue } from "recoil";
-import { DateTime } from "luxon";
+import { atom, useRecoilState } from "recoil";
 
-const currentTime = DateTime.local();
-
-const sortedTodoList = selectorFamily({
-  key: "sortedTodoList",
-  get:
-    (sortedType) =>
-    ({ get }) => {
-      const todos = get(todoList);
-
-      return sortedType === "ASC"
-        ? [...todos].sort((a, b) => a.createdAt - b.createdAt)
-        : todos.slice().sort((a, b) => b.createdAt - a.createdAt);
-    },
-});
-
-const todoList = atom({
-  key: "todoList",
-  default: [
-    { name: "buy milk", createdAt: currentTime },
-    { name: "write a book", createdAt: currentTime.plus({ days: 1 }) },
-    { name: "do some exercise", createdAt: currentTime.plus({ days: 2 }) },
-  ],
+const cityAtom = atom({
+  key: "city",
+  default: "",
 });
 
 function App() {
-  const sortOrder = "DESC";
-  const mySortedTodoList = useRecoilValue(sortedTodoList(sortOrder));
+  const [currentCity, setCurrentCity] = useRecoilState(cityAtom);
 
   return (
     <div className="App">
-      <h1>Todo List</h1>
-      {mySortedTodoList.map((item, index) => (
-        <p key={index}>
-          {item.name} - {item.createdAt.toISODate()}
-        </p>
-      ))}
+      <h1>Enter City</h1>
+      <input
+        type="text"
+        value={currentCity}
+        onChange={(e) => {
+          setCurrentCity(e.target.value);
+        }}
+      />
+      <h4>
+        <div>Weather: </div>
+      </h4>
     </div>
   );
 }
